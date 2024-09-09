a35f727c71fb43d32fa9a18c14c8d9d2337b4ab6
RaihanAnthony <raihanmalay14@gmail.com>
2024-09-09 12:11:56 +0700

    updated cors

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -14,18 +14,14 @@ func CorsMiddlewares(next http.Handler) http.Handler {
 			"https://fe-tb-berkah-jaya-750892348569.us-central1.run.app": true,
 			"http://localhost:3000": true,
 		}
-		fmt.Println("Access-Control-Allow-Origin before:", w.Header().Get("Access-Control-Allow-Origin"))
 
 		if allowedOrigins[origin] {
-			if w.Header().Get("Access-Control-Allow-Origin") == "" {
-				w.Header().Set("Access-Control-Allow-Origin", origin)
-				w.Header().Set("Access-Control-Allow-Method", "GET, POST, PUT, DELETE, OPTIONS")
-				w.Header().Set("Access-Control-Allow-Header", "X-Requested-With, Content-Type, Authorization")
-				w.Header().Set("Access-Control-Allow-Credentials", "true")
-			}
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
 		}
 
-		fmt.Println("Access-Control-Allow-Origin after:", w.Header().Get("Access-Control-Allow-Origin"))
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
